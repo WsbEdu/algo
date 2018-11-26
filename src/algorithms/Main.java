eb1a99872fcbdd56b7909d96bf5f880f39f2fafc
WsbEdu <adrian.olszowski10.10@gmail.com>
2018-11-26 19:31:39 +0100

    uporzadkowanie maina

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -3,7 +3,6 @@ package algorithms;
 import sortAlgorithms.*;
 import java.util.ArrayList;
 import java.util.Random;
-import algorithms.*;
 import sortAlgorithms.BubbleSort;
 import sortAlgorithms.InsertionSort;
 import sortAlgorithms.MergeSort;
@@ -15,16 +14,37 @@ public class Main {
 	public static Sort mergeSort = new MergeSort();
 	
 	public static void main(String[] args) {
-		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		ArrayList<Integer> numbersBubble = new ArrayList<>();
+		ArrayList<Integer> numbersInsert = new ArrayList<>();
+		ArrayList<Integer> numbersMerge = new ArrayList<>();
 		Random r = new Random();
-		for(int i = 0; i <= 100; i++) {
-			numbers.add(r.nextInt(1000));			
+		for(int i = 0; i <= 10000; i++) {
+                        int d = r.nextInt(1000);
+			numbersBubble.add(d);			
+			numbersInsert.add(d);			
+			numbersMerge.add(d);			
 		}
-		System.out.println(numbers);
-		//bubbleSort.sort(numbers);		
-		System.out.println(numbers);
+		System.out.println(numbersBubble);
+                long czasRozpoczecia = System.currentTimeMillis();
+		bubbleSort.sort(numbersBubble);		                
+                long czasZakonczenia = System.currentTimeMillis();
+                long czasTrwania = czasZakonczenia - czasRozpoczecia;
+                System.out.println(numbersBubble);
+		System.out.println(czasTrwania + "ms");
+                
+                
+//		System.out.println(numbersInsert);
 //		insertSort.sort(numbers);
-		mergeSort.sort(numbers);
-                System.out.println(numbers);
+//		czasZakonczenia = System.currentTimeMillis();
+//                czasTrwania = czasZakonczenia - czasRozpoczecia;
+//                System.out.println(czasTrwania + "ms");;
+                
+                System.out.println(numbersMerge);
+                czasRozpoczecia = System.currentTimeMillis();
+		mergeSort.sort(numbersMerge);
+                czasZakonczenia = System.currentTimeMillis();
+                czasTrwania = czasZakonczenia - czasRozpoczecia;
+                System.out.println(numbersMerge);
+                System.out.println(czasTrwania + "ms");
 	}
 }
